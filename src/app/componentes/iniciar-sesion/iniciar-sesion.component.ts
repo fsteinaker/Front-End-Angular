3c2db2fd2ecf9c50dc12eb2bb2a84a1e983bbee9
Federico Steinaker <steinaker@gmail.com>
2022-07-19 22:19:00 -0300

    Desarrollo FRONT-END finalizado, antes de implementar el ng build para enviar el proyecto a Firebase

---
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-iniciar-sesion',
+  templateUrl: './iniciar-sesion.component.html',
+  styleUrls: ['./iniciar-sesion.component.css'],
+})
+export class IniciarSesionComponent {
+  usuario = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService, private router: Router) {}
+  /**
+   * REVISAR, TIENE QUE HACER UN IF PARA VER SI ES CORRECTO EL LOGIN
+   */
+  Ingresar() {
+    console.log(this.usuario);
+    const { email, password } = this.usuario;
+    this.authService.login(email, password).then((res) => {
+      if (email !== '' && password !== '') {
+        if (res) {
+          console.log('res -> ', res);
+          this.router.navigate(['/portfolio']);
+        } else {
+          this.router.navigate(['/error']);
+        }
+      } else {
+        alert('Complete los campos para ingresar');
+      }
+    });
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private afauth: AngularFireAuth, private router: Router) {}
+
+  async login(email: string, password: string) {
+    try {
+      return await this.afauth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+      console.log('error en login: ', err);
+      return null;
+    }
+  }
+
+  async register(email: string, password: string) {
+    try {
+      return (
+        (await this.afauth.createUserWithEmailAndPassword(email, password)) &&
+        this.router.navigateByUrl('/registro-exitoso')
+      );
+    } catch (err) {
+      window.alert('Los datos ingresados están registrados o son incorrectos');
+      return null;
+    }
+  }
+
+  async loginWithGoogle(email: string, password: string) {
+    try {
+      return await this.afauth.signInWithPopup(
+        new firebase.auth.GoogleAuthProvider()
+      );
+    } catch (err) {
+      console.log('error en login con Google: ', err);
+      return null;
+    }
+  }
+
+  // RESETEAR PASSWORD OLVIDADO
+  ForgotPassword(passwordResetEmail: string) {
+    return this.afauth
+      .sendPasswordResetEmail(passwordResetEmail)
+      .then(() => {
+        window.alert(
+          'Se ha mandado un mail a tu cuenta de correo. Verfifica tu casilla SPAM o correo no deseado'
+        );
+        this.router.navigateByUrl('/iniciar-sesion');
+      })
+      .catch(() => {
+        window.alert(
+          'Ingresa una cuenta de correo registrada para restablecer la contraseña'
+        );
+      });
+  }
+
+  getUserLogged() {
+    return this.afauth.authState;
+  }
+
+  logout() {
+    this.afauth.signOut();
+  }
+}
