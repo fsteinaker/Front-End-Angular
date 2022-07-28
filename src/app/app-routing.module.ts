1a7793d60fb012a6ff9fbc40475eb05a0e052144
Federico Steinaker <steinaker@gmail.com>
2022-07-27 22:32:05 -0300

    Corrección responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { IniciarSesionComponent } from './componentes/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './componentes/registrarse/registrarse.component';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
